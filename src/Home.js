3623a5a749b30315e79387900b6ccb5203da52c0
Abhinandan-Khurana <75041733+Abhinandan-Khurana@users.noreply.github.com>
2021-07-31 20:34:38 +0530

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Style.css'
+
+const Home = () => {
+	return (
+		<div>
+			<section className="home" id="Home">
+    <div className="boi">
+        <div className="row">
+            <div className="row1">Hello! My name is</div>
+            <div className="row2">Abhinandan Khurana</div>
+            <div className="row3">And I am a <span className="lernr">Learner!</span></div>
+        </div>
+    </div>
+</section>
+		</div>
+	)
+}
+
+export default Home
