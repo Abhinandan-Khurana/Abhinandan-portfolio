b809e09b8b0568c8f6b1013890021e16bf73842c
Abhinandan-Khurana <75041733+Abhinandan-Khurana@users.noreply.github.com>
2021-07-31 20:34:27 +0530

    Create Contact.js

---
+++ src/Contact.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import './Style.css'
+
+const Contact = () => {
+	return (
+		<div>
+			<section className="contact" id="Contact">
+				<div className="rub">
+
+					<div className="C">
+
+						<div className="cont"><b>Contact Me</b></div>
+						<div className="Myler"><b>—</b><span className="intrLov"><b>Get in Touch</b></span><b>—</b></div>
+
+					</div>
+
+					<div className="alpha">
+
+
+						<div className="plus">
+
+
+
+							<div className="lola">
+								<div className="usr"><i className="fas fa-user"></i></div>
+								<div className="adr"><i className="fas fa-map-marker-alt"></i></div>
+								<div className="eml"><i className="fas fa-envelope"></i></div>
+							</div>
+
+
+							<div className="lola2">
+
+								<div className="adr1">
+									<p><b>Name</b></p>
+									<p>Abhinandan Khurana</p>
+								</div>
+
+								<div className="adr2">
+									<p><b>City</b></p>
+									<p>Gonda</p>
+								</div>
+
+								<div className="adr3">
+									<p><b>Email</b></p>
+									<p>abhinandankhuranaoo7@gmail.com</p>
+								</div>
+
+
+
+							</div>
+
+
+
+
+
+						</div>
+
+						<div className="gggg">
+							<b>
+								<blockquote>If you are persistent, you will get it. If you are consistent, you will keep it.
+								</blockquote>
+							</b>
+							<cite>~Harvey MacKay</cite>
+						</div>
+
+					</div>
+
+				</div>
+
+
+			</section>
+		</div>
+	)
+}
+
+export default Contact
