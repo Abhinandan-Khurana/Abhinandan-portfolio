3255681dc5e51075438201048c7a44753bfdfe9a
Abhinandan-Khurana <75041733+Abhinandan-Khurana@users.noreply.github.com>
2021-07-31 20:34:16 +0530

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Style.css'
+import pic from './image/7.webp'
+const About = () => {
+	return (
+		<div>
+			<section className="about" id="About">
+    <div className="a">
+        <div className="b">
+            <div className="c">
+                <h1>About Me</h1>
+            </div>
+            <div className="d">
+                <p><b>–</b><span className="e"><b>Know me better</b></span><b>–</b></p>
+            </div>
+        </div>
+
+        <div className="f">
+            <div className="g"><img className="img" src={pic} /></div>
+            <div className="h">
+                <div className="i"><b>I am Abhinandan and I am a <span className="lernr">Learner</span></b></div>
+                <div className="j"> I am Cyber Security Researcher @DSC - LPU || Full Stack Web Development Intern @TwoWaits || Coordinator @BLAZE-LPU || CTF Player @N00B_4rMY || B. Tech CSE 24' at @Lovely Professional University || Cyber Security Enthusiast</div>
+                <div className="k"><a href="./CV/Abhinandan's Resume (2).pdf" target="_blank"><button className="btn">Open CV</button></a></div>
+            </div>
+        </div>
+    </div>
+</section>
+		</div>
+	)
+}
+
+export default About
